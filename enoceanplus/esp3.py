1ee08f918cc0627cf2cdd5674f02f45c4286acd9
Damien Braillard <damien.braillard@agileforge.ch>
2020-11-02 08:16:13 +0100

    Update reference of CRC8 used for ESP3 protocol.

--- enoceanplus/esp3.py
+++ enoceanplus/esp3.py
@@ -24,7 +24,9 @@ from typing import Iterable, Tuple, Type, Union
 
 log = logging.getLogger(__name__)
 
-# CRC-8-CCIT polynomial table. Source: https://gist.github.com/hypebeast/3833758
+# CRC-8-CCIT polynomial table using polynomial G(x) = x^8 + x^2 + x^1 + x^0
+# Extracted from the EnOcean serial protocol document "Appendix 3.3 (CRC calculation)"
+#   at https://www.enocean.com/fileadmin/redaktion/pdf/tec_docs/EnOceanSerialProtocol3.pdf
 # fmt: off
 __CRC_TABLE = [
     0x00, 0x07, 0x0e, 0x09, 0x1c, 0x1b, 0x12, 0x15, 0x38, 0x3f, 0x36, 0x31, 0x24, 0x23, 0x2a, 0x2d,
